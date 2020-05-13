314a3e0bc932de9ead311864d5302329f0bab969
Wilco Louwerse <wilco@louwerse.com>
2020-05-13 18:25:59 +0200

    Can add events now :)
    Archief v.0.5

--- api/src/Controller/AgendaController.php
+++ api/src/Controller/AgendaController.php
@@ -86,4 +86,47 @@ class AgendaController extends AbstractController
 
         return $variables;
     }
+
+    /**
+     * @Route("/add")
+     * @Template
+     */
+    public function addAction(Session $session, $slug = false, Request $httpRequest, CommonGroundService $commonGroundService, ApplicationService $applicationService)
+    {
+        $variables = [];
+
+        $variables['calendars'] = [];
+
+        $calendars = $commonGroundService->getResourceList($commonGroundService->getComponent('arc')['href'].'/calendars');
+        if(key_exists("hydra:view", $calendars))
+        {
+            $lastPageCalendars = (int) str_replace("/calendars?page=", "", $calendars["hydra:view"]["hydra:last"]);
+            for ($i = 1; $i <= $lastPageCalendars; $i++)
+            {
+                $variables['calendars'] = array_merge($variables['calendars'], $commonGroundService->getResourceList($commonGroundService->getComponent('arc')['href'].'/calendars', ['page'=>$i])["hydra:member"]);
+            }
+        }
+        else
+        {
+            $variables["calendars"] = $calendars["hydra:member"];
+        }
+
+        if(isset($_POST['Submit']))
+        {
+            $event = [];
+            $event['name'] = $_POST['Name'];
+            $event['description'] = $_POST['Description'];
+            $event['startDate'] = $_POST['StartDate'];
+            $event['endDate'] = $_POST['EndDate'];
+            $calendar = $_POST['Calendar'];
+            if($calendar != "Select Calendar")
+            {
+                $event['calendar'] = $calendar;
+            }
+
+            $commonGroundService->createResource($event, $commonGroundService->getComponent('arc')['href'].'/events');
+        }
+
+        return $variables;
+    }
 }
